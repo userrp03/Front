aef35463f8b1c9dc953c976f9a396d82ef934b3c
Your Name <you@example.com>
2020-11-28 16:53:33 -0300

    Backup co duncionamento de Inclusão

---
+++ model/registro.ts
@@ -0,0 +1,9 @@
+export class Registro {
+    //TODO Editar para campos dos registros - OK
+    id: number;
+    descricao: string;
+    despesa: boolean;
+    imagem: string;
+    tipo: number;
+    dataCriacao: Date;
+  }
